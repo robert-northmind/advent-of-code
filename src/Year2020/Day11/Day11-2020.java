0e9357200cb44d76119691367f33b1a14eff0f9b
Robert Magnusson <robert@northmind.io>
2020-12-11 18:54:11 +0100

    Solved 2020 Day 11 - 2

--- src/Year2020/Day11/Day11-2020.java
+++ src/Year2020/Day11/Day11-2020.java
@@ -19,33 +19,65 @@ class Day_11 {
 }
 
 class SeatingSystem {
+  public interface NextPointMethod {
+    public Point getNexPoint(Point inputPoint);
+  }
+
   HashMap<Point, Seat> seatsMap = new HashMap<Point, Seat>();
 
   SeatingSystem(ArrayList<String> inputStrings) {
-    int y = 0;
+    int yPos = 0;
+    int height = inputStrings.size();
+    int width = 0;
     for (String inputString : inputStrings) {
-      for (int x = 0; x < inputString.length(); x++) {
-        if (inputString.charAt(x) == 'L') {
-          Seat seat = new Seat(new Point(x, y), true);
+      width = inputString.length();
+      for (int xPos = 0; xPos < inputString.length(); xPos++) {
+        if (inputString.charAt(xPos) == 'L') {
+          Seat seat = new Seat(new Point(xPos, yPos), true);
           seatsMap.put(seat.pos, seat);
         }
       }
-      y += 1;
+      yPos += 1;
     }
 
+    // Add neighbour seats
     for (Seat seat : seatsMap.values()) {
       int[] posVals = { -1, 0, 1 };
-      for (int xPos : posVals) {
-        for (int yPos : posVals) {
-          if (xPos == 0 && yPos == 0) {
+      for (int x : posVals) {
+        for (int y : posVals) {
+          if (x == 0 && y == 0) {
             continue;
           }
-          Seat adjacentSeat = seatsMap.get(new Point(seat.pos.x + xPos, seat.pos.y + yPos));
+
+          // Add adjacent seats
+          Seat adjacentSeat = seatsMap.get(new Point(seat.pos.x + x, seat.pos.y + y));
           if (adjacentSeat != null) {
             seat.adjecentSeats.add(adjacentSeat);
           }
+
+          // Add visible seats
+          addVisibleSeats(width, height, seat, new NextPointMethod() {
+            public Point getNexPoint(Point inputPoint) {
+              inputPoint.x += x;
+              inputPoint.y += y;
+              return inputPoint;
+            }
+          });
         }
       }
+    }
+  }
+
+  void addVisibleSeats(int width, int height, Seat seat, NextPointMethod nextPointMethod) {
+    Point nextPoint = nextPointMethod.getNexPoint(new Point(seat.pos.x, seat.pos.y));
+    Seat adjacentSeat = null;
+    while (adjacentSeat == null && nextPoint.x >= 0 && nextPoint.x <= width && nextPoint.y >= 0
+        && nextPoint.y <= height) {
+      adjacentSeat = seatsMap.get(new Point(nextPoint.x, nextPoint.y));
+      nextPoint = nextPointMethod.getNexPoint(nextPoint);
+    }
+    if (adjacentSeat != null) {
+      seat.visibleSeats.add(adjacentSeat);
     }
   }
 
@@ -66,7 +98,8 @@ class SeatingSystem {
       seatingChanged = false;
 
       for (Seat seat : seatsMap.values()) {
-        seat.checkNextState();
+        // seat.checkNextStateAdjacent();
+        seat.checkNextStateVisible();
       }
 
       for (Seat seat : seatsMap.values()) {
@@ -102,6 +135,7 @@ class Seat {
   boolean isFree;
   private boolean isFree_next;
   ArrayList<Seat> adjecentSeats = new ArrayList<Seat>();
+  ArrayList<Seat> visibleSeats = new ArrayList<Seat>();
 
   Seat(Point pos, boolean isFree) {
     this.pos = pos;
@@ -109,32 +143,50 @@ class Seat {
     this.isFree_next = isFree;
   }
 
-  void checkNextState() {
+  void checkNextStateAdjacent() {
+    int adjOccupiedSeats = getNumberAdjacentQccupiedSeats();
     if (isFree) {
-      boolean allAdjacentFree = true;
-      for (Seat adjacentSeat : adjecentSeats) {
-        if (!adjacentSeat.isFree) {
-          allAdjacentFree = false;
-          break;
-        }
-      }
-      if (allAdjacentFree) {
+      if (adjOccupiedSeats == 0) {
         isFree_next = false;
       }
     } else {
-      int occupiedAdjSeats = 0;
-      for (Seat adjacentSeat : adjecentSeats) {
-        if (!adjacentSeat.isFree) {
-          occupiedAdjSeats += 1;
-          if (occupiedAdjSeats >= 4) {
-            break;
-          }
-        }
-      }
-      if (occupiedAdjSeats >= 4) {
+      if (adjOccupiedSeats >= 4) {
         isFree_next = true;
       }
     }
+  }
+
+  void checkNextStateVisible() {
+    int visibleOccupiedSeats = getNumberVisibleQccupiedSeats();
+    if (isFree) {
+      if (visibleOccupiedSeats == 0) {
+        isFree_next = false;
+      }
+    } else {
+      if (visibleOccupiedSeats >= 5) {
+        isFree_next = true;
+      }
+    }
+  }
+
+  private int getNumberVisibleQccupiedSeats() {
+    int visibleOccupiedSeats = 0;
+    for (Seat visibleSeat : visibleSeats) {
+      if (!visibleSeat.isFree) {
+        visibleOccupiedSeats += 1;
+      }
+    }
+    return visibleOccupiedSeats;
+  }
+
+  private int getNumberAdjacentQccupiedSeats() {
+    int adjOccupiedSeats = 0;
+    for (Seat adjacentSeat : adjecentSeats) {
+      if (!adjacentSeat.isFree) {
+        adjOccupiedSeats += 1;
+      }
+    }
+    return adjOccupiedSeats;
   }
 
   boolean updateCurrentState() {
