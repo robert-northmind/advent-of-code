1ea28e030e383d4d79498eb764e8ef812d5ffd82
Robert Magnusson <robert@northmind.io>
2021-12-02 17:20:28 +0100

    Adds time meassuremnt logic

--- src/Year2021/Sources/AdventOfCode2021/AdventOfCode2021.swift
+++ src/Year2021/Sources/AdventOfCode2021/AdventOfCode2021.swift
@@ -6,17 +6,30 @@ public struct AdventOfCode2021 {
 
     public func run() {
         let days: [DailyChallengeRunnable] = [
-            Day1(withConfig: RunInputConfig(type: .testData, day: 1, number: 1)),
-            Day1(withConfig: RunInputConfig(type: .realData, day: 1, number: 1)),
+//            Day1(withConfig: RunInputConfig(type: .testData, day: 1, number: 1)),
+//            Day1(withConfig: RunInputConfig(type: .realData, day: 1, number: 1)),
             
-//            Day2(withConfig: RunInputConfig(type: .testData, day: 2, number: 1)),
-//            Day2(withConfig: RunInputConfig(type: .realData, day: 2, number: 1)),
+            Day2(withConfig: RunInputConfig(type: .testData, day: 2, number: 1)),
+            Day2(withConfig: RunInputConfig(type: .realData, day: 2, number: 1)),
         ]
 
         days.forEach { day in
-            day.runPartOne()
-            day.runPartTwo()
+            runWithTimeMeasurement {
+                day.runPartOne()
+            }
+            runWithTimeMeasurement {
+                day.runPartTwo()
+            }
         }
+    }
+
+    private func runWithTimeMeasurement(_ runClosure: () -> Void) {
+        let start = DispatchTime.now()
+        runClosure()
+        let end = DispatchTime.now()
+        let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds
+        let timeInterval = Double(nanoTime) / 1_000_000_000
+        print("(Execution time: \(timeInterval) seconds)\n")
     }
 }
 
