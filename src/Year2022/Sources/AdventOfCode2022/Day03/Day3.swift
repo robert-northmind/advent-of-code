3abf7d124dbe7fe959fdcf1865d78d6d030446c0
robert@northmind.io <robert@northmind.io>
2022-12-02 16:51:09 +0100

    Prepaares for Day3

---
+++ src/Year2022/Sources/AdventOfCode2022/Day03/Day3.swift
@@ -0,0 +1,21 @@
+//
+//  Day3.swift
+//  
+//
+//  Created by Robert Magnusson on 02.12.22.
+//
+
+import Foundation
+import RegexBuilder
+
+class Day3: DailyChallengeRunnable {
+    override func dayNumber() -> Int { return 3 }
+
+    override func runPartOne() {
+//        let digits = OneOrMore
+        // https://developer.apple.com/videos/play/wwdc2022/110357/
+    }
+    
+    override func runPartTwo() {
+    }
+}
