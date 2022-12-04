b6464423659688deb5fb2cd7e058a7792b09104c
robert@northmind.io <robert@northmind.io>
2022-12-04 19:55:32 +0100

    Prepaares for Day5

---
+++ src/Year2022/Sources/AdventOfCode2022/Day05/Day5.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Robert Magnusson on 04.12.22.
+//
+
+import Foundation
+
+class Day5: DailyChallengeRunnable {
+    override func dayNumber() -> Int { return 5 }
+
+    override func runPartOne() {
+    }
+    
+    override func runPartTwo() {
+    }
+}
