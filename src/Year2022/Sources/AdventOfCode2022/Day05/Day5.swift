a350c32c6a0910e09ea844b58fcfb3c3740b875f
robert@northmind.io <robert@northmind.io>
2022-12-05 11:59:58 +0100

    Solved 2022 Day 05

--- src/Year2022/Sources/AdventOfCode2022/Day05/Day5.swift
+++ src/Year2022/Sources/AdventOfCode2022/Day05/Day5.swift
@@ -6,13 +6,119 @@
 //
 
 import Foundation
+import RegexBuilder
 
 class Day5: DailyChallengeRunnable {
     override func dayNumber() -> Int { return 5 }
 
     override func runPartOne() {
+        var cargoShip = CargoShip(withInputString: inputString)
+        cargoShip.performCraneMovements()
+        let topCrates = cargoShip.getTopCrates()
+        print("The top crates are: \(topCrates.joined(separator: ""))")
     }
     
     override func runPartTwo() {
+        var cargoShip = CargoShip(withInputString: inputString)
+        cargoShip.performCraneMovements9001()
+        let topCrates = cargoShip.getTopCrates()
+        print("The top crates are: \(topCrates.joined(separator: ""))")
+    }
+}
+
+private struct CargoShip {
+    var crateStackMap: [Int: [String]] = [:]
+    var craneMovements: [CraneMovement] = []
+
+    init(withInputString inputString: String) {
+        let inputList = inputString.components(separatedBy: "\n\n")
+        var crateStacksInput = inputList[0].components(separatedBy: "\n")
+        let craneMovementsInput = inputList[1]
+
+        // Create the cargo stacks
+        crateStacksInput.removeLast()
+        crateStacksInput.reverse()
+        
+        let stacksCount = (crateStacksInput[0].count + 1) / 4
+        crateStacksInput.forEach { crateStacksRowString in
+            let crateStacksRow = Array(crateStacksRowString)
+
+            for stackNbr in 0..<stacksCount {
+                let index = 4*stackNbr+1
+                let crate = crateStacksRow[index]
+                if crate != " " {
+                    var crateStack = crateStackMap[stackNbr+1] ?? []
+                    crateStack.append(String(crate))
+                    crateStackMap[stackNbr+1] = crateStack
+                }
+            }
+        }
+        
+        // Create the craneMovements
+        let amountRegex = Capture { OneOrMore(.digit) } transform: { Int($0)! }
+        let regex = Regex {
+            Anchor.startOfLine
+            "move "
+            amountRegex
+            " from "
+            amountRegex
+            " to "
+            amountRegex
+            Anchor.endOfLine
+        }
+        craneMovements = craneMovementsInput.matches(of: regex).map { match in
+            CraneMovement(amount: match.1, from: match.2, to: match.3)
+        }
+    }
+
+    mutating func performCraneMovements() {
+        craneMovements.forEach { craneMovement in
+            (0..<craneMovement.amount).forEach { _ in
+                var fromCrateStack = crateStackMap[craneMovement.from]!
+                var toCrateStack = crateStackMap[craneMovement.to]!
+
+                let crate = fromCrateStack.removeLast()
+                toCrateStack.append(crate)
+
+                crateStackMap[craneMovement.from] = fromCrateStack
+                crateStackMap[craneMovement.to] = toCrateStack
+            }
+        }
+    }
+    
+    mutating func performCraneMovements9001() {
+        craneMovements.forEach { craneMovement in
+            var fromCrateStack = crateStackMap[craneMovement.from]!
+            var toCrateStack = crateStackMap[craneMovement.to]!
+            
+            let lastIndex = fromCrateStack.count
+            let firstIndex = lastIndex - craneMovement.amount
+            
+            let cratesToMove = Array(fromCrateStack[firstIndex..<lastIndex])
+            fromCrateStack = Array(fromCrateStack[0..<firstIndex])
+            toCrateStack.append(contentsOf: cratesToMove)
+
+            crateStackMap[craneMovement.from] = fromCrateStack
+            crateStackMap[craneMovement.to] = toCrateStack
+        }
+    }
+
+    func getTopCrates() -> [String] {
+        var topCrates: [String] = []
+        let stackNumbers = crateStackMap.keys.sorted()
+        stackNumbers.forEach { stackNumber in
+            topCrates.append(crateStackMap[stackNumber]!.last!)
+        }
+        return topCrates
+    }
+}
+
+private struct CraneMovement: CustomStringConvertible {
+    let amount: Int
+    let from: Int
+    let to: Int
+    
+    var description: String {
+        "(Move: \(amount) from: \(from) to: \(to))"
     }
 }
