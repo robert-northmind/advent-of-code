ff7efb00a31b0f3330551d2f3f25e6362c875933
robert@northmind.io <robert@northmind.io>
2022-12-05 12:02:40 +0100

    Prepaares for Day06

--- src/Year2022/Sources/AdventOfCode2022/AdventOfCode2022.swift
+++ src/Year2022/Sources/AdventOfCode2022/AdventOfCode2022.swift
@@ -6,19 +6,19 @@ public struct AdventOfCode2022 {
 
     public func run() {
         let days: [DailyChallengeRunnable] = [
-            Day5(withConfig: InputConfig(type: .testData, number: 1)),
-            Day5(withConfig: InputConfig(type: .realData, number: 1))
+            Day6(withConfig: InputConfig(type: .testData, number: 1)),
+//            Day6(withConfig: InputConfig(type: .realData, number: 1))
         ]
 
         days.forEach { day in
-//            runWithTimeMeasurement {
-//                print("Day: \(day.dayNumber()), Part 1:")
-//                day.runPartOne()
-//            }
             runWithTimeMeasurement {
-                print("Day: \(day.dayNumber()), Part 2:")
-                day.runPartTwo()
+                print("Day: \(day.dayNumber()), Part 1:")
+                day.runPartOne()
             }
+//            runWithTimeMeasurement {
+//                print("Day: \(day.dayNumber()), Part 2:")
+//                day.runPartTwo()
+//            }
         }
     }
 
---
+++ src/Year2022/Sources/AdventOfCode2022/Day06/Day6.swift
@@ -0,0 +1,18 @@
+//
+//  Day6.swift
+//  
+//
+//  Created by Robert Magnusson on 05.12.22.
+//
+
+import Foundation
+
+class Day6: DailyChallengeRunnable {
+    override func dayNumber() -> Int { return 6 }
+
+    override func runPartOne() {
+    }
+    
+    override func runPartTwo() {
+    }
+}
