faaef1492d73129ab42bcd47104246f4448a934f
Robert Magnusson <robert@northmind.io>
2022-12-06 10:00:28 +0100

    Prepares for 2022 Day 07

---
+++ src/Year2022/Sources/AdventOfCode2022/Day07/Day7.swift
@@ -0,0 +1,18 @@
+//
+//  Day7.swift
+//  
+//
+//  Created by Robert Magnusson on 05.12.22.
+//
+
+import Foundation
+
+class Day7: DailyChallengeRunnable {
+    override func dayNumber() -> Int { return 7 }
+
+    override func runPartOne() {
+    }
+
+    override func runPartTwo() {
+    }
+}
