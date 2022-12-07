214a05357496e2983fdb785e1d3db0323cd3281c
Robert Magnusson <robert@northmind.io>
2022-12-07 20:51:02 +0100

    Prepares for 2022 Day 08

---
+++ src/Year2022/Sources/AdventOfCode2022/Day08/Day8.swift
@@ -0,0 +1,18 @@
+//
+//  Day8.swift
+//  
+//
+//  Created by Robert Magnusson on 05.12.22.
+//
+
+import Foundation
+
+class Day8: DailyChallengeRunnable {
+    override func dayNumber() -> Int { return 8 }
+
+    override func runPartOne() {
+    }
+
+    override func runPartTwo() {
+    }
+}
