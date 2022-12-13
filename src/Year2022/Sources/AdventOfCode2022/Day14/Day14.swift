97ac135a6148f2ab4f394845261e5945b563e85b
robert@northmind.io <robert@northmind.io>
2022-12-13 23:57:56 +0100

    Prepares for Day14

---
+++ src/Year2022/Sources/AdventOfCode2022/Day14/Day14.swift
@@ -0,0 +1,19 @@
+//
+//  Day14.swift
+//  
+//
+//  Created by Robert Magnusson on 05.12.22.
+//
+
+import Foundation
+import Collections
+
+class Day14: DailyChallengeRunnable {
+    override func dayNumber() -> Int { return 14 }
+
+    override func runPartOne() {
+    }
+
+    override func runPartTwo() {
+    }
+}
